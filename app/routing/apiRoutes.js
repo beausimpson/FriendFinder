74833ff38faa5741595c7d8214c7eb06dfad6c0d
beausimpson <simpson.beau@gmail.com>
2018-12-22 12:20:55 -0600

    all routes work, can post new friend, there is a bug where the compatibility test does not work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
 
 // routing
 // required data
-var friendData = require("../data/friends");
+var friendData = require("../data/friends.js");
 
 
 module.exports = function (app) {
@@ -10,8 +10,21 @@ module.exports = function (app) {
         res.json(friendData);
     });
 
-    app.post("/api/tables", function(req, res) {
+    app.post("/api/friends", function (req, res) {
+        newFriend = req.body
+        friendData.push(newFriend);
+        res.json(newFriend)
 
+        // newFriend.scores = newFriend.scores.map((x) =>parseInt(x))
+        // TODO: compare newFriend to friendData
+        // console.log(newFriend.scores)
+
+        for (var i = 0; i < friendData.length; i++) {
+            // console.log(friendData[i].scores)
+            for (var j = 0; j < friendData[i].scores.length; j++) {
+                console.log(typeof friendData[i].scores[j])
+            }
+        }
     });
 
 };
