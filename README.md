2d49ee7bbf43e5980855d81df8fd5cd1cc396c3c
beausimpson <simpson.beau@gmail.com>
2018-12-23 11:15:11 -0600

    added info to readme.md, added psuedocode describing needed functionality

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # FriendFinder
+[Friend Finder](https://friendfinder-beausimpson.herokuapp.com/)
+## Built by Beau Simpson
+
+**Full Stack Web Application**
+
+**Built Using**
+- HTML
+- CSS / Bootstrap Framework
+- Node.js
+- Express.js
+- Deployed on Heroku
+
+
+### How to Use:
+
+- To find a new friend, the user will answer the survey questions and submit for matching of a new friend.
+
+- The user's scores will be compared to the database of friends and the closest match will be displayed on screen.
+
+
+- To view the database of friends - 
+[Friend Finder API Link](https://friendfinder-beausimpson.herokuapp.com/api/friends)
+
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,15 +16,19 @@ module.exports = function (app) {
         res.json(newFriend)
 
         // newFriend.scores = newFriend.scores.map((x) =>parseInt(x))
-        // TODO: compare newFriend to friendData
-        // console.log(newFriend.scores)
 
-        for (var i = 0; i < friendData.length; i++) {
-            // console.log(friendData[i].scores)
-            for (var j = 0; j < friendData[i].scores.length; j++) {
-                console.log(typeof friendData[i].scores[j])
-            }
-        }
+        // for (var i = 0; i < friendData.length; i++) {
+        //     // console.log(friendData[i].scores)
+        //     for (var j = 0; j < friendData[i].scores.length; j++) {
+        //         console.log(typeof friendData[i].scores[j])
+        //     }
+        // }
+
+        // TODO: compare newFriend to friendData
+        // take scores from new friend and compare to the scores of the friends in the friendData Array
+        // returns the friend that has the smallest difference in scores
+        // displays the name and picture of closest match in modal on screen 
+
     });
 
 };
